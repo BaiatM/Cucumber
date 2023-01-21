f5228e6e0b7d728538ba33718ddaa7fa73c63de8
Baiat <baiat0215@gmail.com>
2023-01-20 20:30:03 -0500

    html, json, json-html reports r added

--- src/test/java/com/automation/runner/Runner.java
+++ src/test/java/com/automation/runner/Runner.java
@@ -15,7 +15,8 @@ import org.junit.runner.RunWith;
 
         features = "src/test/resources/features",
         glue = "com.automation.steps",
-        tags = "@Baiat"
+        tags = "@Baiat",
+        plugin = {"html:html-report/report.html","json:target/cucumber.json"}
 
 )
 
